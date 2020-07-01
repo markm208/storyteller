67d96f18d4266cccda07684322137bdc56475dd5
Omar <ocataldo@carthage.edu>
2020-07-01 16:05:02 -0500

    Fixed media arrays for no media in a comment

--- core/public/js/playbackInit.js
+++ core/public/js/playbackInit.js
@@ -44,9 +44,7 @@ function AddEventListeners()
     //get the controls
     const stepBackOne = document.getElementById("stepBackOne");
     const stepForwardOne = document.getElementById("stepForwardOne");
-    const restartButton = document.getElementById("restartButton");
     const playbackSlider = document.getElementById("playbackSlider");
-    const highlightButton = document.getElementById("highlightButton");
     const playPauseButton = document.getElementById("playPauseButton");
 
     //Get references to the tabs and where the tabs get their content
@@ -66,25 +64,7 @@ function AddEventListeners()
         step(1);
     });
 
-    //The restart will be depreciated soon
-    restartButton.addEventListener("click", event => {
-        
-        //reset the next event and the slider
-        playbackData.nextEventPosition = 0;
-        playbackSlider.value = 0;
 
-        //for measuring which approach is faster
-        //get the starting timestamp
-        const t0 = performance.now();
-
-        editor.setValue("");
-
-        //get the ending timestamp
-        const t1 = performance.now();
-
-        //print the duration in ms
-        console.log(`Reset took: ${(t1 - t0)} ms`);
-    });
 
     //add event handler to listen for changes to the slider
     playbackSlider.addEventListener("input", event => {
@@ -96,19 +76,7 @@ function AddEventListeners()
         step(playbackSlider.value - playbackData.nextEventPosition);
     });
 
-    highlightButton.addEventListener("click", event =>{
-        //get the selected code
-        //const selection = editor.getSession().getSelection().getRange();
-        const selections = editor.getSession().getSelection().getAllRanges();
 
-        //clear any existing highlights
-        clearHighlights();
-        
-        for (let i = 0; i < selections.length; i++){
-            //add the highlight to the selected code
-            addHighlight(selections[i].start.row, selections[i].start.column, selections[i].end.row, selections[i].end.column);
-        }
-    });
 
     document.querySelector("#addCommentButton").addEventListener("click", event =>{
         
@@ -183,7 +151,7 @@ function AddEventListeners()
             displayAllComments();
 
             //clear out any images uploaded for this comment
-            playbackData.mediaForNewComment = [];
+            playbackData.mediaForNewComment = [[],[],[]];
 
         }
 
