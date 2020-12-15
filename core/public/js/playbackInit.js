66f97511c8e6df439cd2e318c1b7621f80ef45fe
Omar <ocataldo@carthage.edu>
2020-12-15 16:20:28 -0600

    Improved comment

--- core/public/js/playbackInit.js
+++ core/public/js/playbackInit.js
@@ -718,8 +718,8 @@ function setupEventListeners()
             const idOfOriginalCarousel = event.currentTarget.querySelector('[data-returnCarouselId]').getAttribute('data-returnCarouselId');
             const originalCarousel = document.getElementById(idOfOriginalCarousel);
             
-            //if the active src of the expanded modal and the active src of the smaller modal aren't the same,
-            //set the smaller modals active image to the same image as the larger one
+            //if the active src of the expanded carousel and the active src of the smaller carousel are not the same,
+            //set the smaller carousels active image to the same image as the larger one
             if (originalCarousel.querySelector('.carousel-item.active img').getAttribute('src') !== largeModalActiveSrc) {
                 originalCarousel.querySelector('.carousel-item.active img').closest('.carousel-item').classList.remove('active');
                 originalCarouselImages = [...originalCarousel.querySelectorAll('.carousel-item img')];
